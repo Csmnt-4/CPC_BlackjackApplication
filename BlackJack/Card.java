f698e7e6d6fd945ac00ef4e75da34f1a810781c5
Victor Anisimov <anisimoffjr@gmail.com>
2023-03-08 13:14:46 -0400

    Initialised the core classes, added the properties to the "Card" class

---
+++ BlackJack/Card.java
@@ -0,0 +1,178 @@
+enum Suit {Clubs, Diamonds, Hearts, Spades}
+
+enum Rank {Jack, Queen, King, Ace}
+
+public class Card {
+    private String imagePath;
+    private String cardName;
+    private int rankValue;
+    private Rank rank;
+    private Suit suit;
+
+    public Card(int value, int suit) throws Exception {
+        switch (value) {
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+                this.rankValue = value;
+                break;
+            case 11:
+                this.rank = Rank.Jack;
+            case 12:
+                this.rank = Rank.Queen;
+            case 13:
+                this.rank = Rank.King;
+                this.rankValue = 10;
+                break;
+            case 14:
+                this.rankValue = 11;
+                this.rank = Rank.Ace;
+                break;
+            default:
+                throw new Exception("Not a valid card value.");
+        }
+        switch (suit) {
+            case 1 -> {
+                this.suit = Suit.Clubs;
+            }
+            case 2 -> {
+                this.suit = Suit.Diamonds;
+            }
+            case 3 -> {
+                this.suit = Suit.Hearts;
+            }
+            case 4 -> {
+                this.suit = Suit.Spades;
+            }
+            default -> throw new Exception("Not a valid card value.");
+        }
+        if (rank != null) {
+            this.cardName = this.rank.toString() + " of " + this.suit.toString();
+            this.imagePath = this.rank.toString() + this.suit.toString();
+        } else {
+            this.cardName = this.rankValue + " of " + this.suit.toString();
+            this.imagePath = this.rankValue + this.suit.toString();
+        }
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath() {
+        if (this.rank != null) {
+            this.imagePath = this.rank.toString() + this.suit.toString();
+        } else {
+            this.imagePath = this.rankValue + this.suit.toString();
+
+        }
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public void setCardName() {
+        if (this.rank != null) {
+            this.imagePath = this.rank.toString() + this.suit.toString();
+        } else {
+            this.imagePath = this.rankValue + this.suit.toString();
+
+        }
+    }
+
+    public int getValue() {
+        return rankValue;
+    }
+
+    public void setValue(int value) throws Exception {
+        switch (value) {
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+                this.rankValue = value;
+                break;
+            case 11:
+                this.rank = Rank.Jack;
+            case 12:
+                this.rank = Rank.Queen;
+            case 13:
+                this.rank = Rank.King;
+                this.rankValue = 10;
+                break;
+            case 14:
+                this.rankValue = 11;
+                this.rank = Rank.Ace;
+                break;
+            default:
+                throw new Exception("Not a valid card value.");
+        }
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
+
+    public void setRank(int value) throws Exception {
+        switch (value) {
+            case 1 -> this.rank = Rank.Jack;
+            case 2 -> this.rank = Rank.Queen;
+            case 3 -> this.rank = Rank.King;
+            case 4 -> this.rank = Rank.Ace;
+            default -> throw new Exception("Not a valid rank value.");
+        }
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public void setSuit(Suit suit) throws Exception {
+        this.suit = suit;
+    }
+
+    public void setSuit(int value) throws Exception {
+        switch (value) {
+            case 1 -> {
+                this.suit = Suit.Clubs;
+            }
+            case 2 -> {
+                this.suit = Suit.Diamonds;
+            }
+            case 3 -> {
+                this.suit = Suit.Hearts;
+            }
+            case 4 -> {
+                this.suit = Suit.Spades;
+            }
+            default -> throw new Exception("Not a valid card value.");
+        }
+    }
+}
+
+
